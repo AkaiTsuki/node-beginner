91378a0dee292a242a667d3cfe22d11a585ec36e
Jiachi Liu <liu.jiach@husky.neu.edu>
2014-08-29 22:23:08 -0400

    Implemented get and post request

--- node-to-do/server.js
+++ node-to-do/server.js
@@ -5,15 +5,9 @@ var url = require('url');
 function startServer(handle, app){
 	console.log("Server starting...".green);
 	http.createServer(function(req, res){
-
 		var pathname = url.parse(req.url).pathname;
 		var method = req.method;
 		handle(app, pathname, method, req, res);
-
-		res.writeHeader(200, {
-			'Content-Type':'text/plain'
-		});
-		res.end("Hello World");
 	}).listen(3000);
 	console.log("Sever started!".green);
 }
--- node-to-do/todo.js
+++ node-to-do/todo.js
@@ -1,16 +1,29 @@
 var app = require('./app');
+var jade =  require('jade');
+var qs = require('querystring');
 
-// Business logic temporary goes here
+var items = [{'id' : 1, 'text' : 'item1'}, {'id' : 2, 'text' : 'item2'}];
+
 app.get('/', function(req, res){
 	console.log('Handle get request on url /');
+	compileAndShowIndexPage(res);
 });
 
-app.get('/events', function(req, res){
-	console.log('Handle get request on url /events');
+app.get('/items', function(req, res){
+	outputJson(res, items);
 });
 
 app.post('/', function(req, res){
-	console.log('Handle post request on url /');
+	var id = items.length + 1;
+	var postData = '';
+	req.setEncoding('utf8');
+	req.on('data', function(chunk){
+		postData += chunk;
+	});
+	req.on('end', function(){
+		items.push({'id' : id, 'text': qs.parse(postData).item});
+		compileAndShowIndexPage(res);
+	});
 });
 
 app.put('/', function(req, res){
@@ -20,3 +33,19 @@ app.put('/', function(req, res){
 app.delete('/', function(req, res){
 	console.log('Handle delete request on url /');
 });
+
+function compileAndShowIndexPage(res){
+	var fn = jade.compileFile('./index.jade',{'pretty': true});
+	var html = fn({'items' : items});
+	res.writeHeader(200, {
+		'Content-Type' : 'text/html',
+		'Content-Length' : Buffer.byteLength(html)
+	});
+	res.write(html);
+	res.end();
+}
+
+function outputJson(res, obj){
+	res.writeHeader(200, {'Content-Type':'application-json'});
+	res.end(JSON.stringify(obj));
+}
