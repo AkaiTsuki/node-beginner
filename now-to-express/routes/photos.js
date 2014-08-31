0628130620419b617b803b49b6cfccbb44a5cb35
Jiachi Liu <liu.jiach@husky.neu.edu>
2014-08-31 01:05:34 -0400

    Created a photo sharing app with express

---
+++ now-to-express/routes/photos.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var Photo = require('../models/Photo');
+var path = require('path');
+var fs = require('fs');
+var formidable = require('formidable');
+var resolve = path.resolve;
+
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+	Photo.find({},function(err, photos){
+		if(err) return next(err);
+		res.render('photo',{
+			'title': 'Photo Wall',
+			'photos': photos
+		});
+	});
+});
+
+router.get('/upload', function(req, res){
+	res.render('upload', {
+		title: 'Upload Photo'
+	});
+});
+
+router.post('/upload', function(req, res, next){
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(error, fields, files){
+		console.log(fields);
+		console.log(files);
+		var name = fields.photoName;
+		var currentTime = new Date().getTime();
+
+		var path = resolve(__dirname,'../','public/photos', currentTime+'.png');
+		console.log(path);
+
+		fs.rename(files.photoImage.path, path, function(error){
+			if(error){
+				return next(error);
+			}
+
+			Photo.create({
+				'name': name,
+				'path': '/photos/'+ currentTime + '.png'
+			}, function(err){
+				if(err) return next(err);
+				res.redirect('/photos');
+			});
+		});
+	});
+});
+
+function getPhotoAbsPath(){
+	var currentPath = __dirname;
+
+}
+
+module.exports = router;
