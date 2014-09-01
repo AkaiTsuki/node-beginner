a0a7ed729df1ce08ec358e1bb68cc2c7aedd75c9
Jiachi Liu <liu.jiach@husky.neu.edu>
2014-09-01 17:13:07 -0400

    Changed page title

--- socket-chat/routes/index.js
+++ socket-chat/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Chat - Express with Socket.io' });
 });
 
 module.exports = router;
